4fcd81341ffd1dc3ae9f3adae097fe330a12d122
Kazunari93 <aooni0930@gmail.com>
2020-05-24 19:57:12 +0900

    signin

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "mai",
+      email: "mai@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "123",
+      name: "ted",
+      email: "ted@gmail.com",
+      password: "apple",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
 app.get("/", (req, res) => {
   res.send("working");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
 
 app.listen(3000, () => {
